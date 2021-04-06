73650cfb60262e4f4a270cef5b8be9e16970bf87
21600212 <21600212@handong.edu>
2021-04-06 22:51:43 +0900

    study how to develop test code

--- src/main/java/study/studyspring/domain/Member.java
+++ src/main/java/study/studyspring/domain/Member.java
@@ -1,2 +1,23 @@
-package study.studyspring.domain;public class Member {
+package study.studyspring.domain;
+
+public class Member {
+
+    private Long id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
